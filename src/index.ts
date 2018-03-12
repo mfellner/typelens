6a47b89813a34b1d324cd8c5dbca6af98a3ec835
Maximilian Fellner <maximilian.fellner@zalando.de>
2018-03-12 18:00:36 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-interface Indexable {
+export interface Indexable {
   length?: number;
   [n: number]: any;
   [s: string]: any;
 }
 
-type Key = string | number;
+export type Key = string | number;
 
-function getKey<T>(key: keyof T): (obj: T) => T[keyof T] | undefined {
-  return (obj: T) => obj[key];
+function getKeyFn(key: Key, obj: Indexable): any | undefined {
+  if (typeof obj === 'undefined' || obj === null) {
+    return;
+  }
+  return obj[key];
 }
 
-function getPathFn<T extends Indexable>(keys: Array<string | number>, obj: T): any | undefined {
+function getKey(key: Key): (obj: Indexable) => any | undefined {
+  return (obj: Indexable) => getKeyFn(key, obj);
+}
+
+function getPathFn(keys: Key[], obj: Indexable): any | undefined {
   let val: any = obj;
   for (const key of keys) {
     if (typeof val === 'undefined' || val === null) {
@@ -21,13 +28,11 @@ function getPathFn<T extends Indexable>(keys: Array<string | number>, obj: T): a
   return val;
 }
 
-function getPath<T extends Indexable>(keys: Array<string | number>): (obj: T) => any | undefined {
-  return (obj: T) => getPathFn(keys, obj);
+function getPath(keys: Array<string | number>): (obj: Indexable) => any | undefined {
+  return (obj: Indexable) => getPathFn(keys, obj);
 }
 
-function get<T>(key: keyof T): (obj: T) => T[keyof T] | undefined;
-function get<T extends Indexable>(keys: Key[]): (obj: T) => any | undefined;
-function get<T>(arg0: keyof T | Key[]) {
+export function get(arg0: Key | Key[]): (obj: Indexable) => any | undefined {
   if (Array.isArray(arg0)) {
     return getPath(arg0);
   } else {
@@ -35,29 +40,92 @@ function get<T>(arg0: keyof T | Key[]) {
   }
 }
 
+function shallowCopy<T extends Indexable>(obj: T): T {
+  if (Array.isArray(obj)) {
+    return Object.assign([], obj);
+  } else {
+    return Object.assign({}, obj);
+  }
+}
+
 function setKeyFn<T extends Indexable>(key: Key, val: any, obj: T): T | undefined {
   if (typeof obj === 'undefined' || obj === null) {
     return;
   }
-  if (Array.isArray(obj)) {
-    const arr: T = Object.assign([], obj);
-    arr[key] = val;
-    return arr;
+  const copy: T = shallowCopy(obj);
+  copy[key] = val;
+  return copy;
+}
+
+function setKey(key: Key): (val: any) => <T extends Indexable>(obj: T) => T | undefined {
+  return (val: any) => <T extends Indexable>(obj: T) => setKeyFn(key, val, obj);
+}
+
+function setPath(
+  keys: Key[]
+): (val: any) => <T extends Indexable>(obj: T) => Indexable | undefined {
+  return (val: any) => <T extends Indexable>(obj: T) => {
+    if (typeof obj === 'undefined' || obj === null) {
+      return;
+    }
+    const copy = shallowCopy(obj);
+    let currentChild: Indexable = copy;
+    for (let i = 0; i < keys.length - 1; i += 1) {
+      const k = keys[i];
+      if (typeof currentChild[k] === 'undefined' || currentChild[k] === null) {
+        currentChild[k] = typeof keys[i + 1] === 'number' ? [] : {};
+      }
+      currentChild = currentChild[keys[i]];
+    }
+    const lastKey = keys[keys.length - 1];
+    currentChild[lastKey] = val;
+    return copy;
+  };
+}
+
+export function set(
+  arg0: Key | Key[]
+): (val: any) => <T extends Indexable>(obj: T) => Indexable | undefined {
+  if (Array.isArray(arg0)) {
+    return setPath(arg0);
   } else {
-    return Object.assign({}, obj, { [key]: val });
+    return setKey(arg0);
   }
 }
 
-function setKey<T extends Indexable>(key: Key): (val: any) => (obj: T) => T | undefined {
-  return (val: any) => (obj: T) => setKeyFn(key, val, obj);
+function mapFn<T extends Indexable>(fn: (val: T[keyof T]) => any, obj: T): T | undefined {
+  if (typeof obj === 'undefined' || obj === null) {
+    return;
+  }
+  console.log('lets map', fn, obj);
+  const copy = shallowCopy(obj);
+  if (typeof copy.length === 'number') {
+    for (let i = 0; i < copy.length; i += 1) {
+      console.log('key', i, 'set', copy[i], 'to', fn(copy[i]));
+      copy[i] = fn(copy[i]);
+    }
+  } else {
+    for (const k of Object.keys(copy)) {
+      copy[k] = fn(copy[k]);
+    }
+  }
+  return copy;
 }
 
-function setPath<T extends Indexable>(
-  keys: Key[]
-): (val: any) => (obj: T) => Indexable | undefined {
-  return (val: any) => (obj: T) => {
-    const child = getPathFn(keys.slice(0, keys.length - 1), obj);
-    const lastKey = keys[keys.length - 1];
-    return setKeyFn<Indexable>(lastKey, val, child);
-  };
+export function map<T extends Indexable>(fn: (val: T[keyof T]) => any): (obj: T) => T | undefined {
+  return (obj: T) => mapFn(fn, obj);
+}
+
+export type LensFunction<T extends Indexable> = (toFunctor: (x: any) => any) => (obj: T) => T;
+
+export function lens<T extends Indexable>(
+  getter: ((obj: T) => any | undefined),
+  setter: ((val: any) => (obj: T) => any | undefined)
+): LensFunction<T> {
+  return (toFunctor: (x: any) => any) => (target: T) =>
+    mapFn(focus => setter(focus)(target), toFunctor(getter(target)));
+}
+
+export function view<T extends Indexable>(lensFn: LensFunction<T>): (obj: T) => any | undefined {
+  return (obj: T) => lensFn(x => [x])(obj)[0];
 }
