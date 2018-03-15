e2827bf83c41c363b22f6ef7ee326aef52edef08
Maximilian Fellner <max.fellner@gmail.com>
2018-03-15 23:23:56 +0100

    fix: typings

--- src/lens.ts
+++ src/lens.ts
@@ -2,14 +2,14 @@ import { ValueFunctor } from './functors';
 import { Indexable } from './Indexable';
 
 export type ToFunctor<A, B> = (a: A) => ValueFunctor<B>;
-export type Lens<T extends Indexable, A, B> = (
+export type Lens<T extends Indexable, A, B, C> = (
   toFunctor: ToFunctor<A, B>
-) => (obj: T) => ValueFunctor<B>;
+) => (obj: T) => ValueFunctor<C>;
 
-export default function lens<T extends Indexable, A, B>(
+export default function lens<T extends Indexable, A, B, C>(
   get: ((obj: T) => A),
-  set: ((val: B) => (obj: T) => any)
-): Lens<T, A, B> {
+  set: ((val: B) => (obj: T) => C)
+): Lens<T, A, B, C> {
   return (toFunctor: ToFunctor<A, B>) => (target: T) =>
     toFunctor(get(target)).map(focus => set(focus)(target));
 }
--- src/over.ts
+++ src/over.ts
@@ -3,7 +3,7 @@ import { Indexable } from './Indexable';
 import { Lens } from './lens';
 
 export default function over<T extends Indexable, A, B>(
-  lensFn: Lens<T, A, B>
-): (f: ((x: A) => B)) => (obj: T) => B {
+  lensFn: Lens<T, A, B, T>
+): (f: ((x: A) => B)) => (obj: T) => T {
   return (f: ((x: A) => B)) => (obj: T) => lensFn(y => IdentityFunctor(f(y)))(obj).value();
 }
